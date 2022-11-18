c5fc3cf778526026a3454d1fbce7556853df82be
Terrance <terrance.rosas@protonmail.com>
2022-11-18 00:40:20 -0500

    clean code, create helper function in Article_SA

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 import re
-from math import sqrt
 
 import numpy as np
+import PyPDF2.errors
 from PyPDF2 import PdfReader
 
 
 class Article_SA:
-    def __init__(self, filename):
-        self.positive_words = 0
-        self.negative_words = 0
-        self.filename = filename
-        
+    def _count_words(self, filename):
+        """Extract text from PDFs and count Positive / Negative Words"""
         # Extract text from PDF
-        reader = PdfReader(f"./Articles/{filename}")
+        try:
+            reader = PdfReader(f"./Articles/{filename}")
+        except PyPDF2.errors.PdfReadError as error:
+            print(f"File: {filename} may be corrupted and was unable to be read.")
+            print("Continuing Sentiment Analysis...")
+            return
+        
+        # Extract and split PDF text
         text_list_raw = list()
         for page in reader.pages:
             text_list_raw.append(page.extract_text().lower())
@@ -24,35 +28,48 @@ class Article_SA:
         # Get positive and negative words to search for
         positive_array = np.char.lower(np.loadtxt("./Word_Lists/LoughranMcDonald_Positive.csv", dtype="str"))
         negative_array = np.char.lower(np.loadtxt("./Word_Lists/LoughranMcDonald_Negative.csv", dtype="str"))
-
+        
+        # Count Positive / Negative words
         for i in all_words:
-            if i in positive_array:
-                self.positive_words += 1
-            if i in negative_array:
-                self.negative_words += 1
+            if i in positive_array: self.positive_words += 1
+            if i in negative_array: self.negative_words += 1
 
         self.total_words = self.positive_words + self.negative_words
         self.sentiment_score = self.positive_words / \
             (self.positive_words + self.negative_words)
 
+    def __init__(self, filename):
+        self.positive_words = 0
+        self.negative_words = 0
+        self.filename = filename
+        self.total_words = "N/A. Unable to extract text."  # Never updates if PDF is corrupted
+        self.sentiment_score = 0
+        self._count_words(filename)
+    
     def get_filename(self):
+        """Return PDF Filename"""
         return self.filename
 
     def get_positive(self):
+        """Return number of positive words in PDF"""
         return self.positive_words
 
     def get_negative(self):
+        """Return number of negative words in PDF"""
         return self. negative_words
 
     def get_total(self):
+        """Return number of positive and negative words in PDF"""
         return self.total_words
 
     def get_sentiment(self):
+        """Return Sentiment Analysis Score"""
         return self.sentiment_score
 
 
 class Sentiment_Analysis:
-    def _insertion_sort(self, articles):
+    def _insertion_sort(self, articles): # Helper function to sort PDFs ascending
+        """Sort PDFs in nondescending-order"""
         unordered_articles = articles
         for k in range(1, len(unordered_articles)):
             cur = unordered_articles[k]
@@ -66,6 +83,9 @@ class Sentiment_Analysis:
 
     def __init__(self):
         article_filenames = os.listdir("./Articles/")
+        for i in range(len(article_filenames) - 1):  # Filter non-PDF file extensions
+            if article_filenames[i][-3:].lower() != "pdf": article_filenames.pop(i)
+
         self.articles = list()  # Contains individual article SA
         for i in range(len(article_filenames)):
             self.articles.append(Article_SA(article_filenames[i]))
@@ -73,11 +93,10 @@ class Sentiment_Analysis:
         self._insertion_sort(self.articles)
 
     def report_SA(self):
+        """Print Sentiment Analysis Report."""
         print()
-        print(
-            "============================================================================")
-        print(
-            "             Article Sentiment Analysis Score in Ascending Order            ")
+        print("============================================================================")
+        print("             Article Sentiment Analysis Score in Ascending Order            ")
         for article in self.ordered_articles:
             print(f"Filename: {article.get_filename()}")
             print(f"Positive WC: {article.get_positive()}")
@@ -85,8 +104,7 @@ class Sentiment_Analysis:
             print(f"Total WC: {article.get_total()}")
             print(f"SA Score: {article.get_sentiment()}")
             print()
-        print(
-            "============================================================================")
+        print("============================================================================")
         print()
 
 
